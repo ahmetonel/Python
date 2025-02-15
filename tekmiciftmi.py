0ce5bc0ccbc95130b24c193ef348254013970067
Ahmet <82672020+ahmetonel@users.noreply.github.com>
2025-02-15 18:00:28 +0300

    Add files via upload

---
+++ tekmiciftmi.py
@@ -0,0 +1,20 @@
+print("Tek mi cift mi")
+n1=int(input("Bir sayi girin: "))
+if n1%2==0:
+    print("cift sayi")
+else:
+    print("tek sayi")
+
+
+n2=int(input("devam etmek icin 1 gir...   cikis yapmak icin 0 gir...  "))
+
+while n2==1:
+   n1=int(input("Bir sayi girin: "))
+   if n1%2==0:
+    print("cift sayi")
+   else:
+    print("tek sayi")
+
+   n2=int(input("devam etmek icin 1..."))
+
+print("Cikis yapildi...")                                                       #Ahmet ONEL
